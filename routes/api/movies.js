af991f76d19c99911b32fcaf29cc97f957ad8666
jonnahmarie <jonnah.marie@gmail.com>
2021-05-03 23:32:05 -0700

    adding express routing

---
+++ routes/api/movies.js
@@ -0,0 +1,13 @@
+const router = require('express').Router();
+const movieController = require('../../controllers/movieControllers');
+
+router.route('/')
+    .get(movieController.findAll)
+    .post(movieController.create);
+
+router.route('/:id')
+    .get(movieController.findById)
+    .put(movieController.update)
+    .delete(movieController.remove);
+
+module.exports = router;
