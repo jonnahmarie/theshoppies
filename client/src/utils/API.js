0408ef0f3a15589351d18a8858ad42af8b59eca7
jonnahmarie <jonnah.marie@gmail.com>
2021-05-04 15:44:56 -0700

    added current nomination code

--- client/src/utils/API.js
+++ client/src/utils/API.js
@@ -1,19 +1,21 @@
-import axios from 'axios';
+import axios from "axios";
 
-export default {
-    getMovie: query => {
-        return axios.get(`http://www.omdbapi.com/?apikey={process.env.REACT_APP_OMDB_KEY}&t=${query}`);
-    },
-    deleteMovie: async (id) => {
-        const result = await axios.delete('/api/movies/' + id);
-        return result.data;
-    },
-    addMovie: async movieData => {
-        const result = await axios.post('/api/movies', movieData);
-        return result.data;
-    },
-    viewMovies: async () => {
-        const result = await axios.get('/api/movies');
-        return result.data;
-    }
+const API = {
+  getMovie: query => {
+    return axios.get(`http://www.omdbapi.com/?apikey=${process.env.REACT_APP_OMDB_KEY}&t=${query}`);
+  },
+  // Deletes the book with the given id
+  deleteMovie: id => {
+    return axios.delete("/api/movies/" + id).then(result => result.data);
+  },
+  // Saves a book to the database
+  addMovie: movieData => {
+    return axios.post("/api/movies", movieData).then(result => result.data);
+  },
+  // Get the saved a books from the database
+  nominatedMovies: () => {
+    return axios.get("/api/movies").then(result => result.data);
+  }
 };
+
+export default API;
